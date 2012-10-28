8dab238f870ee45ad40f1abc8fe56e555395fcfa
Sean Hogan <seagaia@gmail.com>
2012-10-28 17:53:30 -0500

    interfaces with user mem stuff in syscall.c

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -12,9 +12,28 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+static void syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  unsigned call_nr;
+  int args[3]; // 3 args max
+  memset (args, 0, sizeof args);
+  copy_in (&call_nr, f->esp, sizeof call_nr);
+
+  // We know there are only 3 args max to a system call, fetch 'em later
+  // If they're pointers we'll just deference them later
+  copy_in (args, (uint32_t *) f->esp + 1, 3);
+
+  int retval;
+  switch (call_nr) {
+    case SYS_WRITE:
+      retval = write(args[0],(const void *) args[1], (unsigned) args[2]);
+      break;
+    case SYS_HALT:
+      break;
+    default:
+      break;
+  }
+  f->eax = retval;
 }
+
+
