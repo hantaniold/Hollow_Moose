790e2cf022f624bbf7274b973cdf9de9da913f3d
Charles Ian Ormsby Cary <cioc@cicero.cs.uchicago.edu>
2012-11-13 21:36:17 -0600

    added some infrastructure for adding a lazy loading, compiles but needs testing

--- pintos/src/vm/frame-table.c
+++ pintos/src/vm/frame-table.c
@@ -1,17 +1,28 @@
 #include <list.h>
 #include <hash.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "threads/palloc.h"
 #include "threads/malloc.h"
 #include "threads/vaddr.h"
+#include "threads/thread.h"
 #include "threads/synch.h"
 #include "userprog/pagedir.h"
 #include "vm/frame-table.h"
+#include "filesys/directory.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
 
 //FRAME TABLE GLOBALS
 static struct hash frame_table;
 static struct lock frame_table_lock;
 
+//LOCAL FUNCTIONS
+static frame_table_entry *find_closest(struct thread *t, void *access);
+static bool set_page(struct thread *t, void * upage, void *frame);
+
 //FORWARD DEFS
+//TODO - THESE EXTRA DEFS MIGHT NOT BE NECESSARY
 unsigned thread_hash(const struct hash_elem *e, void *aux); 
 bool thread_less(const struct hash_elem *a, const struct hash_elem *b, void *aux);
 void clear_frame_table(struct thread *t);
@@ -42,7 +53,7 @@ thread_less(const struct hash_elem *a, const struct hash_elem *b, void *aux)
   {
     frame_table_entry *fe_a = hash_entry(a, frame_table_entry, elem);
     frame_table_entry *fe_b = hash_entry(b, frame_table_entry, elem);
-    if (fe_a->frame < fe_b->frame) 
+    if (fe_a->upage < fe_b->upage) 
     {
       return true;
     } else 
@@ -59,6 +70,7 @@ frame_table_init(void)
   hash_init(&frame_table, thread_hash, thread_less, NULL);
 }
 
+/* actually allocates and installs a user page.  NOT LAZY */
 void *
 install_user_page(struct thread *t, void *upage, enum palloc_flags flags UNUSED)
 {
@@ -73,24 +85,139 @@ install_user_page(struct thread *t, void *upage, enum palloc_flags flags UNUSED)
   fe->tid = t->tid;
   fe->frame = frame;
   fe->upage = upage; 
+  fe->on_swap = false;
+  fe->zeroes = false;
+
   lock_acquire(&frame_table_lock);
   t->stack_pages += 1;
   hash_insert(&frame_table, &fe->elem);
-  if (pagedir_get_page (t->pagedir, upage) == NULL)
-  {
-    //printf("HERE1\n");
-    bool result = pagedir_set_page (t->pagedir, upage, frame, true);
-    //printf("RESULT %i\n", (int)result);
-  }
-  else
+  bool result = set_page(t, upage, frame);
+  lock_release(&frame_table_lock);
+  
+  if (!result)  
   {
     palloc_free_page(frame);
     return NULL;
   }
-  lock_release(&frame_table_lock);
 
   return frame;
 }
+
+/* lazy load a page from t->exec_lock file at pos and read bytes size*/
+void
+lazy_install_user_page(struct thread *t, 
+                       void *upage, 
+                       int32_t pos, 
+                       uint32_t size,
+                       bool zeroes)
+{
+  frame_table_entry *fe = (frame_table_entry *)malloc(sizeof(frame_table_entry));
+  fe->tid = t->tid;
+  fe->upage = upage;
+  fe->pos = pos;
+  fe->size = size;
+  fe->zeroes = zeroes;
+  fe->on_swap = false;
+  
+  lock_acquire(&frame_table_lock);
+  hash_insert(&frame_table, &fe->elem);
+  lock_release(&frame_table_lock);
+}
+
+//TODO - NEED TO MOVE EXEC_LOCK WHEN YOU MODIFY PROCESS FOR LAZY LOADING
+
+
+/* Called from page fault to actually load a page that was
+ * setup via lasy_install_user_page
+ */
+void *
+load_lazy_page(struct thread *t, void *upage)
+{
+  frame_table_entry *entry = find_closest(t, upage);
+  if (entry != NULL)
+  {
+    if (entry->zeroes)
+    {
+       void *frame = palloc_get_page(PAL_USER | PAL_ZERO);
+       if (frame == NULL)
+       {
+         return NULL;
+       }
+       
+       bool result = set_page(t, upage, frame);
+       if (!result)
+       {
+         palloc_free_page(frame);
+         return NULL;
+       }
+       return frame;
+    }
+    else
+    {
+      void *frame = palloc_get_page(PAL_USER | PAL_ZERO);
+      if (frame == NULL)
+      {
+        return NULL;
+      }
+
+      struct file *exec_file = t->exec_lock;    
+      lock_acquire(&frame_table_lock);
+      file_seek(exec_file, entry->pos);
+      file_read(exec_file, frame, (off_t)entry->size);
+      lock_release(&frame_table_lock);
+      
+      bool result = set_page(t, upage, frame);
+      if (!result)
+      {
+        palloc_free_page(frame);
+        return NULL;
+      }
+      return frame;
+    }
+  }
+  else 
+  {
+    return NULL;
+  }
+}
+
+//finds the page that contains access of thread t
+//returns null if no such page
+static frame_table_entry*
+find_closest(struct thread *t, void *access)
+{
+  struct list *bucket = find_bucket_by_index(&frame_table, hash_int(t->tid));
+  if (bucket != NULL)
+  {
+    struct list_elem *i;
+    i = list_begin(bucket);
+    while (i != list_end(bucket))
+    {
+      struct hash_elem *hi = list_elem_to_hash_elem (i);
+      frame_table_entry *entry = hash_entry(hi, frame_table_entry, elem);
+      if (entry->upage <= access && access <= (entry->upage + PGSIZE))
+      {
+        return entry;
+      }
+    }
+  }
+  return NULL;
+}
+
+//Private method to simplify adding a page to a page table
+static bool
+set_page(struct thread *t, void * upage, void *frame)
+{
+  if (pagedir_get_page (t->pagedir, upage) == NULL)
+  {
+    return pagedir_set_page (t->pagedir, upage, frame, true);
+  }
+  else
+  {
+    return false;
+  }
+}
+
 
 bool
 is_on_stack(void *access, void *esp) 
@@ -104,6 +231,7 @@ is_on_stack(void *access, void *esp)
   return false;
 }
 
+/* TODO - TEST/DEBUG THIS */
 void
 clear_frame_table(struct thread *t)
 {
--- pintos/src/vm/frame-table.h
+++ pintos/src/vm/frame-table.h
@@ -19,7 +19,8 @@ typedef struct {
   uint32_t fd; // In case this page is stored in some file
   int32_t pos; // Position the page is read from in the file
   uint32_t size; // nr bytes we care about (might not need..?)
-   
+  bool zeroes; //set to true if the page is all zeroes
+
   bool on_swap; // Has this page been written to swap
   block_sector_t sector_nr; // What swap spot it's on
 
@@ -37,6 +38,21 @@ void *get_user_page(struct thread *t, void *upage, enum palloc_flags flags);
 //false otherwise
 bool is_on_stack(void *access, void *esp); 
 
+//Invoke to deallocate all user pages of a thread t
 void clear_frame_table(struct thread *t);
+
+//Sets up a user page so that it can lazily loaded later by a call
+//to load_lazy_page
+void lazy_install_user_page(struct thread *t, 
+                       void *upage, 
+                       int32_t pos, 
+                       uint32_t size,
+                       bool zeroes);
+
+//Loads the page that was initializef via lazy_install_user_page
+//whose upage is closest to access
+//Returns NULL if some issue occured.  Null indicates page not loaded
+void *load_lazy_page(struct thread *t, void *access);
+
 
 #endif
