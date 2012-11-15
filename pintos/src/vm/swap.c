e84ba55c4ace7f7eeb67183e9dc8b80d1b2cf484
Sean Hogan <seagaia@gmail.com>
2012-11-15 00:48:14 -0600

    added swap_write, doesn't work yet need to plug in with page struct

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -1,47 +1,74 @@
 #include <stdbool.h>
+#include <stddef.h>
 #include "vm/swap.h"
 #include "devices/block.h"
 #include "bitmap.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
 
 static struct bitmap * swap_table;
+static struct lock swap_lock;
+static struct block * swap_block;
 
+#define PAGE_SECTORS (PGSIZE / BLOCK_SECTOR_SIZE)
 
-/* Find an empty sector to be used to be swapped
- * via lookup into the swap table. Return -1
- * on failure, the sector number otherwise */
-int32_t
-swap_find_empty_sector (void) 
+static void swap_write (size_t sector_nr, void * p);
+// Find space for the page on disk (8 sectors), write it to disk.
+bool 
+swap_out (void * p) 
 {
-  const struct bitmap * st = swap_table;
-  int i;
-  int32_t entries = bitmap_size(swap_table);
-  // LINEAR TIME BABY
-  for (i = 0; i < entries; i++) 
+  size_t swap_sector = 0;
+
+  lock_acquire (&swap_lock);
+  if ((swap_sector == bitmap_scan (swap_table, 0, 1, false)) == SIZE_MAX) 
   {
-    if (false == bitmap_test(swap_table, i))
-    {
-      return i;
-    }
+    lock_release (&swap_lock);
+    return false;
   }
-  return -1;
+  lock_release (&swap_lock);
+  swap_write (swap_sector, p);
+  return true;
 }
 
 
-/* Write to the swap?? */
+// Read data, clear swap and bitmap
 bool
-swap_write(int32_t sector_nr)
+swap_read (void * p)
 {
-  // Somehow write to swap
-  // Return sector blah blah
-  return false;
+}
+// Given a "sector_nr" (which is actually indexing groups of PAGE_SECTORS
+// sectors, write p's datap ortion to disk and set it
+//
+static void
+swap_write (size_t sector_nr, void * p)
+{
+  // Some attribute of p should have PAGE_SIZE bytes.
+  // p->sector_nr = sector_nr;
+  const void * data = p;
+  int i;
+  for (i = 0; i < PAGE_SECTORS; i++) 
+  {
+    // There is internal synch.  in this call so we don't lock
+    block_write (swap_block, sector_nr*PAGE_SECTORS + i, data + BLOCK_SECTOR_SIZE*i);
+  }
 }
 
 void
 swap_init(void) 
 {
-  struct block * swap = block_get_role (BLOCK_SWAP);
-  block_sector_t nr_swap_sectors  = block_size (swap);
-  swap_table = bitmap_create (nr_swap_sectors);
+  swap_block = block_get_role (BLOCK_SWAP);
+  if (swap_block == NULL) 
+  {
+    printf ("NO SWAP!!!! SWAP DISABLED\n");
+    swap_table = bitmap_create (0);
+  }
+  else 
+  {
+    block_sector_t nr_swap_sectors  = block_size (swap_block);
+    // Dear god note that 8 block sectors = 1 page sector
+    swap_table = bitmap_create (nr_swap_sectors / PAGE_SECTORS);
+    lock_init (&swap_lock);
+  }
 
 }
 
--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -1,4 +1,5 @@
 #include "devices/block.h"
-int32_t swap_find_empty_sector(void);
-bool swap_write (int32_t sector_nr);
+#include <stddef.h>
+bool swap_out (void * p);
+bool swap_read (void * p);
 void swap_init (void);
