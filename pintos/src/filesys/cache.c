dbb3cf629874cc6935bcac23cc354fb6061db6d4
Charles Ian Ormsby Cary <cioc@cicero.cs.uchicago.edu>
2012-12-01 19:48:52 -0600

    lots of helper functions for buffer cache

--- pintos/src/filesys/cache.c
+++ pintos/src/filesys/cache.c
@@ -9,10 +9,6 @@
 
 #define INVALID_SECTOR ((block_sector_t) -1)
 
-
-
-
-
 struct cache_block 
   {
     struct lock block_lock;
@@ -31,13 +27,18 @@ struct cache_block
 #define CACHE_CNT 64
 struct cache_block cache[CACHE_CNT];
 struct lock cache_sync;
-static int hand = 0;
+static uint32_t hand = 0;
 
 
 static void flushd_init (void);
 static void readaheadd_init (void);
 static void readaheadd_submit (block_sector_t sector);
 
+static struct cache_block *in_cache(block_sector_t sector);
+static struct cache_block *find_empty();
+static struct cache_block *try_to_empty();
+static void flush_block(struct cache_block *cb);
+static void clear_block(struct cache_block *cb);
 
 /* Initializes cache. */
 void
@@ -51,15 +52,92 @@ cache_init (void)
     lock_init(&cb->data_lock);
     cond_init(&cb->no_readers_or_writers);
     cond_init(&cb->no_writers);
-    cb->readers = 0;
-    cb->read_waiters = 0;
-    cb->writers = 0;
-    cb->write_waiters = 0;
-    cb->sector = 0;
-    cb->up_to_date = true;
-    cb->dirty = false;
+    clear_block(cb);
   }
   flushd_init();
+}
+
+//cache_sync must already be acquired
+struct cache_block * 
+in_cache(block_sector_t sector)
+{
+  int i;
+  for (i = 0; i < CACHE_CNT; ++i){
+   struct cache_block *cb = &cache[i];
+   if (cb->sector == sector) {
+     return cb;
+   }
+  }
+  return NULL;
+}
+
+//cache_sync must already be acquired
+struct cache_block *
+find_empty()
+{
+  int i;
+  for (i = 0; i < CACHE_CNT; ++i){
+   struct cache_block *cb = &cache[i];
+   if (cb->sector == -1) {
+     return cb;
+   }
+  }
+  return NULL; 
+}
+
+//cache_sync must already be acquired
+//TODO - NEED TO ACCOUNT FOR value of metadata versus data
+struct cache_block *
+try_to_empty()
+{
+  uint32_t hand_start = hand;
+   
+  for (; hand < CACHE_CNT; ++hand){
+   struct cache_block *cb = &cache[hand];
+   if ( cb->readers == 0 &&
+        cb->read_waiters == 0 &&
+        cb->writers == 0 &&
+        cb->write_waiters == 0) {
+    flush_block(cb); 
+    return cb;
+   }
+  }
+  hand = 0;
+  for (; hand < hand_start; ++hand) {
+    struct cache_block *cb = &cache[hand];
+    if ( cb->readers == 0 &&
+        cb->read_waiters == 0 &&
+        cb->writers == 0 &&
+        cb->write_waiters == 0) {
+      flush_block(cb); 
+      return cb;
+    }
+  }
+  return NULL;  
+}
+
+void 
+flush_block(struct cache_block *cb)
+{
+  lock_acquire(&cb->data_lock);
+  if (cb->dirty && cb->sector > INVALID_SECTOR)
+  {
+    block_write(fs_device, cb->sector, cb->data);
+  }
+  lock_release(&cb->data_lock);
+}
+
+
+void 
+clear_block(struct cache_block *cb)
+{
+  cb->readers = 0;
+  cb->read_waiters = 0;
+  cb->writers = 0;
+  cb->write_waiters = 0;
+  cb->sector = -1;
+  cb->up_to_date = true;
+  cb->dirty = false;
 }
 
 /* Flushes cache to disk. */
@@ -70,24 +148,7 @@ cache_flush (void)
   int i;
   for (i = 0; i < CACHE_CNT; ++i) {
     struct cache_block *cb = &cache[i];
-    
-    //unsure about the locking here
-    lock_acquire(&cb->block_lock);
-    cond_wait(&cb->no_readers_or_writers, &cb->block_lock);
-    lock_acquire(&cb->data_lock);
-    if (cb->dirty)
-    {
-      block_write(fs_device, cb->sector, cb->data);
-    }
-    lock_release(&cb->data_lock); 
-    cb->readers = 0;
-    cb->read_waiters = 0;
-    cb->writers = 0;
-    cb->write_waiters = 0;
-    cb->sector = 0;
-    cb->up_to_date = true;
-    cb->dirty = false;
-    lock_release(&cb->block_lock);
+    flush_block(cb);   
   }
   lock_release(&cache_sync);
 }
@@ -106,29 +167,45 @@ cache_lock (block_sector_t sector, enum lock_type type)
   int i;
 
   try_again:
-
+  lock_acquire(&cache_sync);
   /* Is the block already in-cache? */
+  struct cache_block *cb;
+  cb = in_cache(sector);
+  if (cb != NULL) {
+    //TODO - LOCKS AND SUCH
+    lock_release(&cache_sync); 
+    return cb;
+  } else {
+    /* Not in cache.  Find empty slot. */
+    cb = find_empty(); 
+    if (cb != NULL) {
+      //TODO - LOCKS AND SUCH
+      lock_release(&cache_sync);
+      return cb;
+    } else {
+      /* No empty slots.  Evict something. */
+      cb = try_to_empty();
+      if (cb != NULL) {
+        //TODO - LOCKS AND SUCH
+        lock_release(&cache_sync);
+        return cb;
+      } else {
+        /* Wait for cache contention to die down. */
 
-  /* Not in cache.  Find empty slot. */
+        // sometimes, you might get into a situation where you
+        // cannot find a block to evict, or you cannot lock
+        // the desired block. If that's the case there might
+        // some contention. So the safest way to do this, is to
+        // release the cache_sync lock, and sleep for 1 sec, and
+        // try again the whole operation.
 
-  /* No empty slots.  Evict something. */
-
-
-
-
-  /* Wait for cache contention to die down. */
-
-  // sometimes, you might get into a situation where you
-  // cannot find a block to evict, or you cannot lock
-  // the desired block. If that's the case there might
-  // some contention. So the safest way to do this, is to
-  // release the cache_sync lock, and sleep for 1 sec, and
-  // try again the whole operation.
-
-  lock_release (&cache_sync);
-  timer_msleep (1000);
-  goto try_again;
-}
+        lock_release (&cache_sync);
+        timer_msleep (1000);
+        goto try_again;
+      } //try_to_empty 
+    } //find_empty
+  } //in_cache
+} //end cache_lock
 
 /* Bring block B up-to-date, by reading it from disk if
    necessary, and return a pointer to its data.
