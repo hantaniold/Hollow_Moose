13c1d6b8258f9ffc0ef9f401796147b0952475e2
Sean Hogan <seagaia@gmail.com>
2012-12-09 11:46:14 -0600

    UNDO inumber

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -892,13 +892,7 @@ sys_isdir (int fd)
 static int
 sys_inumber(int fd)
 {
-  thread_fs_lock ();
-  struct file *target = thread_get_file(fd);
-  if (target == NULL) return -1;
-  struct inode *check_inode = file_get_inode(target);
-  if (check_inode == NULL) return -1;
-  thread_fs_unlock  ();
-  return inode_get_inumber (check_inode);
+  return -1;
 }
 
 
